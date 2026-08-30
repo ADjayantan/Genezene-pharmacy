e579abf656d5f842b5457df439748e1fa3bff23e
ADjayantan <adjayantan2007@gmail.com>
2026-08-30 22:38:08 +0530

    Fix mobile banner cropping by using native aspect ratio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -139,7 +139,7 @@ export default async function Home() {
                 <picture>
                   <source media="(max-width: 767px)" srcSet={banner.mobileSrc} />
                   <source media="(min-width: 768px)" srcSet={banner.src} />
-                  <img src={banner.src} alt={banner.alt} className="w-full aspect-[10/4] sm:aspect-[21/6] md:aspect-[1205/320] object-cover rounded-md" />
+                  <img src={banner.src} alt={banner.alt} className="w-full aspect-[1205/320] object-cover rounded-md" />
                 </picture>
               </Link>
             ))}
